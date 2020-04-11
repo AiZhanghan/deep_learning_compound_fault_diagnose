490ff250ba7aa238d52c18a1dabd4e208676b693
黄振凯 <35103759+AiZhanghan@users.noreply.github.com>
2020-04-11 18:15:50 +0800

    1

--- code/Trainer.py
+++ code/Trainer.py
@@ -3,8 +3,6 @@ import time
 import torch
 import numpy as np
 
-import utils
-
 
 class Trainer:
     '''
@@ -44,7 +42,7 @@ class Trainer:
             records: 训练记录
         '''
         # 网络初始化
-        self.net.apply(utils.weight_init)
+        self.net.apply(self._weight_init)
         self.net = self.net.to(self.device)
         self.net = self.net.float()
         # net输出层未经过sigmoid, loss选BCEWithLogitsLoss
@@ -97,13 +95,48 @@ class Trainer:
 
             print('time %.1f sec' % (time.time() - start))
         
-        self._save(records, log)
+        predict_dic = self._predict(valid_iter, test_iter)
+        self._save(records, predict_dic, log)
+        
         return self.net, records
+
+    def _predict(self, valid_iter, test_iter):
+        """预测验证集和测试集数据
+        Args:
+            valid_iter: 验证集数据迭代器
+            test_iter: 测试集数据迭代器
+        Return:
+            predict_dic: dict{str: np.array}
+        """
+        self.net.eval()
+        y_hat = np.array([])
+        y = np.array([])
+
+        with torch.no_grad():
+            for x, y_batch in valid_iter:
+                y_hat_batch = (self.net(x.to(self.device))).cpu().numpy()
+                if y_hat.size == 0:
+                    y_hat = y_hat_batch
+                    y = y_batch
+                else:
+                    y_hat = np.concatenate((y_hat, y_hat_batch))
+                    y = np.concatenate((y, y_batch))
+            if test_iter:
+                for x, y_batch in test_iter:
+                    y_hat_batch = (self.net(x.to(self.device))).cpu().numpy()
+                    y_hat = np.concatenate((y_hat, y_hat_batch))
+                    y = np.concatenate((y, y_batch))
+        self.net.train()
+        predict_dic = {}
+        predict_dic["y_hat"] = y_hat
+        predict_dic["y"] = y
+        return predict_dic
     
-    def _save(self, records, log_):
-        """保存训练过程acc以及训练好的模型
+    def _save(self, records, predict_dic, log_):
+        """保存训练过程acc, 训练好的模型, 验证集测试集预测值
         Args:
             records: dict[list]
+            predict_dic: dict{str: np.array}
             log_: str, 辅助日志
         """
         if not os.path.exists("./file"):
@@ -116,7 +149,9 @@ class Trainer:
         log = "_%s_%d_%f_%f_%d_%s" % (log_, self.num_epochs, 
             self.learning_rate, self.weight_decay, self.batch_size, now)
         np.savez(os.path.join("./file", "acc_records" + log), **records)
-        torch.save(self.net, os.path.join("./file", "model%s.pkl" % log))
+        np.savez(os.path.join("./file", "predict_dic" + log), **records)
+        # torch.save(self.net, os.path.join("./file", "model%s.pkl" % log))
+
 
     def _evaluate_accuracy(self, data_iter):
         '''评估指标，准确率'''
@@ -132,3 +167,17 @@ class Trainer:
                 n += y.shape[0]
         self.net.train()
         return acc_sum / n
+
+    def _weight_init(self, m):
+        '''根据网络层的不同定义不同的初始化方式'''
+        if isinstance(m, torch.nn.Linear):
+            torch.nn.init.xavier_normal_(m.weight)
+            torch.nn.init.constant_(m.bias, 0)
+        # 也可以判断是否为conv1d，使用相应的初始化方式   
+        elif isinstance(m, torch.nn.Conv1d):
+            torch.nn.init.kaiming_normal_(m.weight, mode='fan_out',
+                nonlinearity='relu')
+        # 是否为批归一化层
+        elif isinstance(m, torch.nn.BatchNorm1d):
+            torch.nn.init.constant_(m.weight, 1)
+            torch.nn.init.constant_(m.bias, 0)
--- code/utils.py
+++ code/utils.py
@@ -1,21 +1,6 @@
 import torch
 
 
-def weight_init(m):
-    '''根据网络层的不同定义不同的初始化方式'''
-    if isinstance(m, torch.nn.Linear):
-        torch.nn.init.xavier_normal_(m.weight)
-        torch.nn.init.constant_(m.bias, 0)
-    # 也可以判断是否为conv1d，使用相应的初始化方式   
-    elif isinstance(m, torch.nn.Conv1d):
-        torch.nn.init.kaiming_normal_(m.weight, mode='fan_out',
-            nonlinearity='relu')
-    # 是否为批归一化层
-    elif isinstance(m, torch.nn.BatchNorm1d):
-        torch.nn.init.constant_(m.weight, 1)
-        torch.nn.init.constant_(m.bias, 0)
-
-
 def get_stride_padding(l_in):
     '''辅助确定CNN模型结构, 根据信号长度求解stride, padding，
     Args:
